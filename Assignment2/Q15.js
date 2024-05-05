014514aa07f5edca3316ff56f09151cdddaa0543
m4Jestyk <atharvjoshi372002@gmail.com>
2024-05-06 01:45:45 +0530

    first commit

---
+++ Assignment2/Q15.js
@@ -0,0 +1,7 @@
+function updateClock() {
+  const now = new Date();
+  console.log(
+    `${now.getHours().toFixed(0)}:${now.getMinutes()}:${now.getSeconds()}`
+  );
+}
+setInterval(updateClock, 1000);
